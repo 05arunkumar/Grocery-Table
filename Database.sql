d1eba5dde7d1ddc3e081a35e07f1c981e914139f
05arunkumar <105204807+05arunkumar@users.noreply.github.com>
2025-03-10 11:05:13 +0530

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,4 +1,5 @@
 
+
 create table material (
   material_id int primary key,
 name varchar(50) not null,
@@ -10,4 +11,21 @@ create table customer (
   customer_id int primary key,
   name varchar(50) not null,
   phone varchar (15) unique
-  );
+);
+
+create table order_details(
+  order_id int,
+  material_id int,
+  quantity int,
+  primary key (order_id, material_id),
+  foreign key (order_id)
+  orders (order_id), 
+  foreign key (material_id)
+);
+
+
+
+
+
+
+
