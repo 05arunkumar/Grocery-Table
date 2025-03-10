983ae14822b4ddb4baaf373949c1f3dab6fdf4e4
05arunkumar <105204807+05arunkumar@users.noreply.github.com>
2025-03-10 10:40:50 +0530

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -5,3 +5,9 @@ name varchar(50) not null,
 price decimal (10,2) not null,
 quantity int not null
 );
+
+create table customer (
+  customer_id int primary key,
+  name varchar(50) not null,
+  phone varchar (15) unique
+  );
